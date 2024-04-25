c55da976550bb7ea5088a2de1c457acaa5e8947e
varushchi <vadim.shchigolev@mail.ru>
2024-04-25 17:32:04 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import MovieCard from './MovieCard';
+import './App.css'
 
 function App() {
+  const [movieData, setMovieData] = React.useState(null);
+  React.useEffect(() => {
+    const apiKey = '80a00ae154cf4596b7bac7d29180daf0';
+    const url = 'https://api.themoviedb.org/3/movie/popular?api_key=' + apiKey;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setMovieData(data.results))
+      .catch(error => {
+        console.error('Error fetching data:', error)
+      })
+  }, []);
+
+  console.log(movieData);
+
+  const movieElem = movieData && movieData.map(elem => {
+    return(
+      <MovieCard 
+        key = {elem.id}
+        id = {elem.id}
+        img = {`https://image.tmdb.org/t/p/w500${elem.poster_path}`}
+        title = {elem.title}
+        rating = {elem.vote_average.toFixed(1)}
+        date = {elem.release_date.split("-").join('.')}
+        overview = {elem.overview}
+      />
+    )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {movieData && movieElem}
+    </main>
   );
 }
+
+
 
 export default App;
---
+++ src/MovieCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function MovieCard(props)
+{
+  const [isCliked, setIsClicked] = React.useState(false)
+
+  function toggle()
+  {
+    setIsClicked(prev => !prev)
+  }
+
+  return(
+    <div className='movie-card' id={props.id} onClick={toggle}>
+      <div className='img-div'>
+        <img src={props.img} alt={props.title} className='movie-img'/>
+        <p className='movie-rating'>{props.rating}</p>
+        <p className='movie-overview'>{isCliked && props.overview}</p>
+      </div>
+      <p className='movie-title'>{props.title}</p>
+      <p className='movie-date'>{props.date}</p>
+      
+    </div>
+  )
+}
+
+export default MovieCard
